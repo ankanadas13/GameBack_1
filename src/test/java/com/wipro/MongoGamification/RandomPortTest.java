1756eeb26ad03ca6b58a6b89df37f06ac7d4b1d8
Ankana Das <50711571+ankanadas13@users.noreply.github.com>
2019-08-16 11:02:30 +0530

    Update RandomPortTest.java

--- src/test/java/com/wipro/MongoGamification/RandomPortTest.java
+++ src/test/java/com/wipro/MongoGamification/RandomPortTest.java
@@ -28,7 +28,7 @@ public class RandomPortTest {
   {
       RestTemplate restTemplate = new RestTemplate();
        
-      final String baseUrl = "http://localhost:"+randomServerPort+"/";
+      final String baseUrl = "http://ec2-52-66-245-186.ap-south-1.compute.amazonaws.com:8080";
       URI uri = new URI(baseUrl);
    
      
